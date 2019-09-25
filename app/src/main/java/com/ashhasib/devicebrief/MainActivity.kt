47462697c82eac50e58c37a33fe8c3eb63bc578e
AshHasib <ashhasib54@gmail.com>
2019-09-25 23:48:09 +0600

    Collapsible Toolbar Added in Device Fragment

--- app/src/main/java/com/ashhasib/devicebrief/MainActivity.kt
+++ app/src/main/java/com/ashhasib/devicebrief/MainActivity.kt
@@ -2,13 +2,17 @@ package com.ashhasib.devicebrief
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.MemoryFile
+import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.fragment.app.Fragment
+import com.ashhasib.devicebrief.fragment.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNav : BottomNavigationView
-
+    private lateinit var fragment: Fragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,36 +21,64 @@ class MainActivity : AppCompatActivity() {
         bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
 
 
+        initComponents()
+
         setupBottomNav()
 
     }
 
 
 
+
+
+
+
+
+
+
+    private fun initComponents() {
+        val deviceFragment = DeviceFragment()
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, deviceFragment)
+            .commit()
+    }
+
+
+
     private fun setupBottomNav() {
+
+        var fragment = Fragment()
+
         bottomNav.setOnNavigationItemSelectedListener {
 
             when(it.itemId) {
                 R.id.bottom_nav_system -> {
-
+                    fragment = DeviceFragment()
                 }
 
                 R.id.bottom_nav_cpu -> {
-
+                    fragment = CPUFragment()
                 }
 
                 R.id.bottom_nav_memory -> {
-
+                    fragment = MemoryFragment()
                 }
 
                 R.id.bottom_nav_networks -> {
-
+                    fragment = NetworkFragment()
                 }
 
                 R.id.bottom_nav_apps -> {
-
+                    fragment = AppsFragment()
                 }
             }
+
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit()
 
 
             return@setOnNavigationItemSelectedListener true
--- app/src/main/java/com/ashhasib/devicebrief/fragment/DeviceFragment.kt
+++ app/src/main/java/com/ashhasib/devicebrief/fragment/DeviceFragment.kt
@@ -1,2 +1,48 @@
 package com.ashhasib.devicebrief.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ashhasib.devicebrief.R
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.CollapsingToolbarLayout
+
+
+class DeviceFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_device, container, false)
+
+        setupCollapsingLayout(view)
+
+        return view
+    }
+
+    fun setupCollapsingLayout(view: View) {
+        val collapsibleLayout = view.findViewById(R.id.collapsibleLayout) as CollapsingToolbarLayout
+        val appBar = view.findViewById(R.id.appBar) as AppBarLayout
+        appBar.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+            internal var isShow = false
+            internal var scrollRange = -1
+
+            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.totalScrollRange
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsibleLayout.title  = "Device"
+                    isShow = true
+                } else if (isShow) {
+                    collapsibleLayout.title = ""
+                    isShow = false
+                }
+            }
+        })
+
+    }
+}
